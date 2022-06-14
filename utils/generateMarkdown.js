559cb1c8d6d9deeaf019938ce98f69767e299904
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-06-14 11:59:15 -0400

    add link to license section on license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  return `![License Badge](https://img.shields.io/badge/license-${license.length > 0 ? license.split('-')[0] : 'none'}-blue)`
+  return `[![License Badge](https://img.shields.io/badge/license-${license.length > 0 ? license.split('-')[0] : 'none'}-blue)](#license)`
 }
 
 // Create a function that returns the license link
